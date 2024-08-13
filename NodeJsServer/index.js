213abb67f8371a3226a7a9f039b36b0bf9058b6b
Gremliss <liss.przemek@gmail.com>
2024-08-13 15:41:19 +0200

    Added User Name

--- NodeJsServer/index.js
+++ NodeJsServer/index.js
@@ -4,20 +4,29 @@ const http = require("http");
 const WebSocket = require("ws");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
 wss.on("connection", function connection(ws) {
   ws.on("message", function incoming(message, isBinary) {
-    console.log("NEW MESSAGE:");
-    console.log(message.toString(), isBinary);
+    const parsedMessage = JSON.parse(message.toString());
+    const messageWithSender = {
+      sender: parsedMessage.sender,
+      text: parsedMessage.text,
+    };
+
+    console.log("NEW MESSAGE:", messageWithSender, isBinary);
+
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
+        client.send(JSON.stringify(messageWithSender));
       }
     });
   });
 });
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 server.listen(8080, () => {
   console.log("Listening to port: 8080");
 });
--- WebsocketChat/app/index.tsx
+++ WebsocketChat/app/index.tsx
@@ -10,21 +10,22 @@ import {
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-// const colorScheme = useColorScheme();
-
 export default function App() {
   const [serverState, setServerState] = React.useState<string>("Loading...");
   const [messageText, setMessageText] = React.useState<string>("");
+  const [userName, setUserName] = React.useState<string>("");
+  const [chosenUserName, setChosenUserName] = React.useState<string>("");
   const [disableButton, setDisableButton] = React.useState<boolean>(true);
   const [inputFieldEmpty, setInputFieldEmpty] = React.useState<boolean>(true);
-  const [serverMessages, setServerMessages] = React.useState<string[]>([]);
+  const [inputUserFieldEmpty, setInputUserFieldEmpty] = React.useState<boolean>(true);
+  const [serverMessages, setServerMessages] = React.useState<{sender: string, text: string}[]>([]);
   var ws = React.useRef(new WebSocket("ws://192.168.0.236:8080/")).current;
 
   const colorScheme = useColorScheme() || 'light';
   const styles = createStyles(colorScheme);
 
   React.useEffect(() => {
-    const serverMessagesList: string[] = [];
+    const serverMessagesList: {sender: string, text: string}[] = [];
     ws.onopen = () => {
       setServerState("Connected to the server.");
       setDisableButton(false);
@@ -37,17 +38,54 @@ export default function App() {
       setServerState(e.message || "An error occurred.");
     };
     ws.onmessage = (e) => {
-      serverMessagesList.push(e.data);
+      const message = JSON.parse(e.data);
+      serverMessagesList.push(message);
       setServerMessages([...serverMessagesList]);
     };
   }, []);
   const submitMessage = () => {
-    ws.send(messageText);
+    const message = {
+      sender: chosenUserName,
+      text: messageText,
+    };
+    ws.send(JSON.stringify(message));
     setMessageText("");
     setInputFieldEmpty(true);
   };
   return (
     <View style={styles.container}>
+      {chosenUserName === "" ? (
+
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <TextInput
+          style={{
+            borderWidth: 1,
+            borderColor: Colors[colorScheme].tint,
+            flexGrow: 1,
+            padding: 5,
+            color: Colors[colorScheme].text,
+          }}
+          placeholder={"User name"}
+          placeholderTextColor={Colors[colorScheme].text}
+          onChangeText={(uName) => {
+            setUserName(uName);
+            setInputUserFieldEmpty(uName.length > 0 ? false : true);
+          }}
+          value={userName}
+        />
+        <Button
+          onPress={() => setChosenUserName(userName)}
+          title={"Submit"}
+          disabled={disableButton || inputUserFieldEmpty}
+        />
+      </View>
+
+         ) : null}
+
       <View
         style={{
           height: 30,
@@ -69,14 +107,20 @@ export default function App() {
         }}
       >
         <ScrollView>
-          {serverMessages.map((item, ind) => {
-            return <Text
-        style={{
-          color: Colors[colorScheme].text,
-        }} key={ind}>{item}</Text>;
-          })}
+          {serverMessages.map((item, ind) => (
+            <Text
+              key={ind}
+              style={{
+                color: Colors[colorScheme].text,
+              }}
+            >
+              {item.sender}: {item.text}
+            </Text>
+          ))}
         </ScrollView>
       </View>
+      {chosenUserName != "" ? (
+
       <View
         style={{
           flexDirection: "row",
@@ -85,7 +129,7 @@ export default function App() {
         <TextInput
           style={{
             borderWidth: 1,
-            borderColor: "black",
+            borderColor: Colors[colorScheme].tint,
             flexGrow: 1,
             padding: 5,
             color: Colors[colorScheme].text,
@@ -104,6 +148,7 @@ export default function App() {
           disabled={disableButton || inputFieldEmpty}
         />
       </View>
+      ) : null}
     </View>
   );
 }
