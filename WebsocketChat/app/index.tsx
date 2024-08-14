1dda43d3e5c14bdcf95dd184e69bccef7408fdf6
Gremliss <liss.przemek@gmail.com>
2024-08-14 14:55:05 +0200

    Redux added

--- WebsocketChat/app/index.tsx
+++ WebsocketChat/app/index.tsx
@@ -7,25 +7,33 @@ import {
   Button,
   ScrollView,
 } from "react-native";
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { store } from "./redux/store";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { setName } from "./redux/userSlice";
+import { RootState } from "./redux/store";
 
-export default function App() {
+function MainComponent() {
   const [serverState, setServerState] = React.useState<string>("Loading...");
   const [messageText, setMessageText] = React.useState<string>("");
-  const [userName, setUserName] = React.useState<string>("");
+  const userName = useSelector((state: RootState) => state.user.name);
+  const dispatch = useDispatch();
   const [chosenUserName, setChosenUserName] = React.useState<string>("");
   const [disableButton, setDisableButton] = React.useState<boolean>(true);
   const [inputFieldEmpty, setInputFieldEmpty] = React.useState<boolean>(true);
-  const [inputUserFieldEmpty, setInputUserFieldEmpty] = React.useState<boolean>(true);
-  const [serverMessages, setServerMessages] = React.useState<{sender: string, text: string}[]>([]);
-  var ws = React.useRef(new WebSocket("ws://192.168.0.236:8080/")).current;
+  const [inputUserFieldEmpty, setInputUserFieldEmpty] =
+    React.useState<boolean>(true);
+  const [serverMessages, setServerMessages] = React.useState<
+    { sender: string; text: string }[]
+  >([]);
+  let ws = React.useRef(new WebSocket("ws://192.168.0.236:8080/")).current;
 
-  const colorScheme = useColorScheme() || 'light';
+  const colorScheme = useColorScheme() || "light";
   const styles = createStyles(colorScheme);
 
   React.useEffect(() => {
-    const serverMessagesList: {sender: string, text: string}[] = [];
+    const serverMessagesList: { sender: string; text: string }[] = [];
     ws.onopen = () => {
       setServerState("Connected to the server.");
       setDisableButton(false);
@@ -43,113 +51,85 @@ export default function App() {
       setServerMessages([...serverMessagesList]);
     };
   }, []);
+
+  const submitUserName = () => {
+    setChosenUserName(userName);
+  };
+
   const submitMessage = () => {
     const message = {
-      sender: chosenUserName,
+      sender: userName,
       text: messageText,
     };
     ws.send(JSON.stringify(message));
     setMessageText("");
     setInputFieldEmpty(true);
   };
+
   return (
     <View style={styles.container}>
       {chosenUserName === "" ? (
-
-      <View
-        style={{
-          flexDirection: "row",
-        }}
-      >
-        <TextInput
-          style={{
-            borderWidth: 1,
-            borderColor: Colors[colorScheme].tint,
-            flexGrow: 1,
-            padding: 5,
-            color: Colors[colorScheme].text,
-          }}
-          placeholder={"User name"}
-          placeholderTextColor={Colors[colorScheme].text}
-          onChangeText={(uName) => {
-            setUserName(uName);
-            setInputUserFieldEmpty(uName.length > 0 ? false : true);
-          }}
-          value={userName}
-        />
-        <Button
-          onPress={() => setChosenUserName(userName)}
-          title={"Submit"}
-          disabled={disableButton || inputUserFieldEmpty}
-        />
-      </View>
-
-         ) : null}
-
-      <View
-        style={{
-          height: 30,
-          backgroundColor: Colors[colorScheme].statusBG,
-          padding: 5,
-        }}
-      >
-        <Text
-        style={{
-          color: Colors[colorScheme].text,
-        }}
-        >{serverState}</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: Colors[colorScheme].backgroundChat,
-          padding: 5,
-          flexGrow: 1,
-        }}
-      >
-        <ScrollView>
-          {serverMessages.map((item, ind) => (
-            <Text
-              key={ind}
-              style={{
-                color: Colors[colorScheme].text,
-              }}
-            >
-              {item.sender}: {item.text}
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder={"User name"}
+            placeholderTextColor={Colors[colorScheme].text}
+            onChangeText={(uName) => {
+              dispatch(setName(uName));
+              setInputUserFieldEmpty(uName.length > 0 ? false : true);
+            }}
+            value={userName}
+          />
+          <Button
+            onPress={submitUserName}
+            title={"Submit"}
+            disabled={inputUserFieldEmpty}
+          />
+        </View>
+      ) : (
+        <>
+          <View style={styles.statusBar}>
+            <Text style={{ color: Colors[colorScheme].text }}>
+              {serverState}
             </Text>
-          ))}
-        </ScrollView>
-      </View>
-      {chosenUserName != "" ? (
-
-      <View
-        style={{
-          flexDirection: "row",
-        }}
-      >
-        <TextInput
-          style={{
-            borderWidth: 1,
-            borderColor: Colors[colorScheme].tint,
-            flexGrow: 1,
-            padding: 5,
-            color: Colors[colorScheme].text,
-          }}
-          placeholder={"Add Message"}
-          placeholderTextColor={Colors[colorScheme].text}
-          onChangeText={(text) => {
-            setMessageText(text);
-            setInputFieldEmpty(text.length > 0 ? false : true);
-          }}
-          value={messageText}
-        />
-        <Button
-          onPress={submitMessage}
-          title={"Submit"}
-          disabled={disableButton || inputFieldEmpty}
-        />
-      </View>
-      ) : null}
+          </View>
+          <View style={styles.chatContainer}>
+            <ScrollView>
+              {serverMessages.map((item, ind) => (
+                <Text key={ind} style={{ color: Colors[colorScheme].text }}>
+                  {item.sender}: {item.text}
+                </Text>
+              ))}
+            </ScrollView>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.textInput}
+              placeholder={"Add Message"}
+              placeholderTextColor={Colors[colorScheme].text}
+              onChangeText={(text) => {
+                setMessageText(text);
+                setInputFieldEmpty(text.length > 0 ? false : true);
+              }}
+              value={messageText}
+            />
+            <Button
+              onPress={submitMessage}
+              title={"Submit"}
+              disabled={disableButton || inputFieldEmpty}
+            />
+          </View>
+        </>
+      )}
     </View>
+  );
+}
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <MainComponent />
+    </Provider>
   );
 }
 
@@ -160,6 +140,26 @@ const createStyles = (color: keyof typeof Colors) => {
       backgroundColor: Colors[color].background,
       paddingTop: 30,
       padding: 8,
+    },
+    inputContainer: {
+      flexDirection: "row",
+    },
+    textInput: {
+      borderWidth: 1,
+      borderColor: Colors[color].tint,
+      flexGrow: 1,
+      padding: 5,
+      color: Colors[color].text,
+    },
+    statusBar: {
+      height: 30,
+      backgroundColor: Colors[color].statusBG,
+      padding: 5,
+    },
+    chatContainer: {
+      backgroundColor: Colors[color].backgroundChat,
+      padding: 5,
+      flexGrow: 1,
     },
   });
 };
